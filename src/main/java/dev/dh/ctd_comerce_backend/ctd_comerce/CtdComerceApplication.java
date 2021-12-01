fb93ba7e9f9bd8127434b0c58ce2b0b80aaa4d4a
aloisiot <aloisiot.c98@gmail.com>
2021-12-01 12:13:21 -0300

    primeiro commit

---
+++ src/main/java/dev/dh/ctd_comerce_backend/ctd_comerce/CtdComerceApplication.java
@@ -0,0 +1,13 @@
+package dev.dh.ctd_comerce_backend.ctd_comerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtdComerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtdComerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/dh/ctd_comerce_backend/ctd_comerce/CtdComerceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dh.ctd_comerce_backend.ctd_comerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtdComerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
